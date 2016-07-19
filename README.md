5b9d6661739decfac97b3cc9c7e786a927e63383
arthur <plc_4art@hotmail.com>
2016-07-19 11:14:50 -0300

    server configuration

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # starter-js-angularjs
 Starter Project for AngularJs Application
+
+Follow up plan(not particular route)
+
+* install dependencies
+* html5 mode
+* file upload
+* resource integration
+* http interceptor
+* ui-router
+* gulp task runner tasks
+* crop image
+
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    path = require('path'),
+    app = express();
+
+
+app.use(/\/(bower_components|src|dist)\/[\s\S]+?\.(css|js|jpg|jpeg|png|map)$/, (req, res) => res.sendFile(path.join(__dirname, req.originalUrl)));
+app.use(/\/views\/[\s\S]+?\.html$/, (req, res) => res.sendFile(path.join(__dirname, app.get('env') === 'development' ? 'src' : 'dist', req.originalUrl)));
+app.get(/(\/{1}.*)?$/, (req, res) => res.sendFile(path.join(__dirname, app.get('env') === 'development' ? 'src' : 'dist', 'index.html')));
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: app.get('env') === 'development' ? err : {}
+    });
+});
+
+app.listen(3001, function () {
+    console.log(`Server listening on port ${3001}`);
+});

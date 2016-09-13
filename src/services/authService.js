622451297211911163a5f044111ac6c2842c658b
arthur <plc_4art@hotmail.com>
2016-09-13 17:08:03 -0300

    authentication bearer implementation

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,17 +5,14 @@ angular.module('StarterAngular')
                 return $http.post(urlBase + 'register', user);
             },
             login: function (user) {
-                return $http({
-                    method: "OPTIONS",
-                    url: urlBase + 'login',
-                    data: 'username=harthur26&password=3yskmdbU&grant_type=password'
-                }).then(function (d) {
-                    console.log(d);
-                    // var base64 = window.btoa(user.Username + ':' + user.Password);
-                    // $cookies.put("starter_user", base64);
-                    // $http.defaults.headers.common.Authorization = "Basic " + base64;
-                    // return d;
-                });
+                return $http.post(urlBase + 'login', 'username='+user.Username+'&password='+user.Password+'&grant_type=password')
+                    .then(function (d) {
+                        console.log(d);
+                        var base64 = window.btoa(d.data.access_token);
+                        $cookies.put("starter_user", base64);
+                        $http.defaults.headers.common.Authorization = "Bearer " + d.data.access_token;
+                        return d;
+                    });
             },
             current: function () {
                 return $http.get(urlBase + 'currentuser', null);
